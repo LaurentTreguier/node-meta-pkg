77277f49d09391b3e31877f113fa1d5adfa5e56a
LaurentTreguier <laurent@treguier.org>
2016-12-02 13:05:05 +0100

    Clean up out directory

--- out/backend.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import * as util from './util';
-class Backend {
-    get available() {
-        return util.checkExistence(this.command) && this.platforms.indexOf(process.platform) !== -1;
-    }
-}
-export default Backend;
